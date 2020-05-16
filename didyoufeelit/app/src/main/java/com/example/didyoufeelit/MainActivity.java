1bc820ee31a9192958bd127ce7fd9e772f26286f
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-05-16 17:46:54 +0530

    Problem solved in manifest--><uses-permission android:name="android.permission.INTERNET"/>
    A practice app to understand networking

--- didyoufeelit/app/src/main/java/com/example/didyoufeelit/MainActivity.java
+++ didyoufeelit/app/src/main/java/com/example/didyoufeelit/MainActivity.java
@@ -49,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         protected Event doInBackground(String... urls) {
+
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
             Event result = Utils.fetchEarthquakeData(urls[0]);
             return result;
         }
@@ -61,6 +67,12 @@ public class MainActivity extends AppCompatActivity {
          * (which was returned from the doInBackground() method) and update the views on the screen.
          */
         protected void onPostExecute(Event result) {
+
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+
             updateUi(result);
         }
     }
